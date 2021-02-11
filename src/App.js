a30db14045816c9f2e55afae07be1edb7ec54e26
consoledot <bimbooladele@outlook.com>
2021-02-11 19:31:05 +0100

    created signin and form input component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
+import SignInAndOutPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component'
 import {Switch, Route} from 'react-router-dom'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage}/>
         <Route path="/shop" component={ShopPage}/>
+        <Route path="/signin" component={SignInAndOutPage}/>
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-out/sign-in-and-sign-out.component.jsx
@@ -0,0 +1,11 @@
+
+import './sign-in-and-sign-out.style.scss'
+import Signin from '../../components/sign-in/sign-in.component'
+
+const SignInAndOutPage =()=>(
+    <div className='sign-in-and-sign-out'>
+        <Signin/>
+    </div>
+)
+
+export default SignInAndOutPage
