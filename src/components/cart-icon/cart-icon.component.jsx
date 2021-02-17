4960d760bbaebc9803d3b9bfd3d8a3d872e9a2a6
consoledot <bimbooladele@outlook.com>
2021-02-17 17:31:49 +0100

    added my cartitems to drop down and cartitems count

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,17 +2,21 @@ import './cart-icon.style.scss'
 import {connect} from 'react-redux'
 import {toggleCartHidden} from '../../redux/cart/cart.action'
 
+
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
 
-const CartIcon = ({toggleCartHidden})=>(
+const CartIcon = ({toggleCartHidden, cartItems})=>(
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className="shopping-icon"/>
-        <span className="item-count">0</span>
+        <span className="item-count">{cartItems.length}</span>
 
     </div>
 )
 const mapDispatchToProps= dispath=>({
     toggleCartHidden: ()=> dispath(toggleCartHidden())
 })
+const mapStateToProps = ({cart:{cartItems}})=>({
+    cartItems
+})
 
-export default connect(null, mapDispatchToProps)(CartIcon)
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
