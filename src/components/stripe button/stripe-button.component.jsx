f5a3faa17a0061fd6c72c2e27c1a85548f1f71ad
consoledot <bimbooladele@outlook.com>
2021-04-02 22:15:24 +0100

    added stripe checkout

---
+++ src/components/stripe button/stripe-button.component.jsx
@@ -0,0 +1,26 @@
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51ITvs6CxDTml9mGSsL2YDx9X1hUthj7u3QW43jMmu4RiyuN6iqp8hWNs8lYlsNSLb47ZtpNOs9kooxcr1Vsh7is500P1fuXNlY";
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total price is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+export default StripeCheckoutButton;
